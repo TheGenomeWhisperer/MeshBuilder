139b288b03e20251abcb91c113ae25389c0329ea
TheGenomeWhisperer <topping@email.arizona.edu>
2016-05-08 17:45:33 -0700

    Major logic update... Node bridge logic.etc.

--- Mesh.cs
+++ Mesh.cs
@@ -11,7 +11,7 @@
 |               Note: Mesh for Mesh Manager
 |               Note: The very last coordinate of all the Vector3 arrays is a "special" node that is built on the in-game Mesh
 |
-|               Last Update: May 2nd, 2016
+|               Last Update: May 4th, 2016
 |
 */  
 	
@@ -39,6 +39,13 @@ public class Mesh
     {
         // Draenor ContinentID
         // All Zone IDs for Continent!
+        
+        // Dungeon Mesh Checking
+        if (QH.IsInInstance())
+        {
+            return DM.GetDungeonMesh();
+        }
+        
         if (currentCont == 1116)
         {
             if (zoneID == 6720 || zoneID == 6868 || zoneID == 6745 || zoneID == 6849 || zoneID == 6861 || zoneID == 6864 || zoneID == 6848 || zoneID == 6875 || zoneID == 6939 || zoneID == 7005 || zoneID == 7209 || zoneID == 7004 || zoneID == 7327 || zoneID == 7328 || zoneID == 7329)
@@ -102,6 +109,7 @@ public class Mesh
         }
         
         // No matches
+        API.Print("No Custom Meshes Have Been Found at Your Location.  Please submit a request in the forums!"); 
         Vector3[] result = new Vector3[]{};
         return result;
     }
@@ -252,7 +260,7 @@ public class Mesh
         
     // Method:              "GetClosestNode(Vector3[] hotspots, Vector3 destination)"
     // This is to be used with the "GetMesh" method
-    private static Vector3 GetClosestNode(Vector3[] hotspots, Vector3 destination)
+    public static Vector3 GetClosestNode(Vector3[] hotspots, Vector3 destination)
     {
         Vector3 closest = hotspots[0];
         for (int i = 1; i < hotspots.Length - 1; i++)
@@ -267,53 +275,10 @@ public class Mesh
         
     
     
-    // Method:             "HotspotGenerator(int seconds)"
-    // What it Does:        Run this method whilst running a path in the game to build the actual mesh.
-    public static IEnumerable<int> HotspotGenerator(int seconds)
-    {
-        string result = ("Vector3[] hotspots = new Vector3[]{{");
-        int count = seconds * 2;
-        int index = 0;
-        Vector3 currentPosition = API.Me.Position;
-        Vector3 tempPosition = currentPosition;
-        
-        while (index < count)
-        {
-            // Verifying Current Position has changed from the last one.
-            tempPosition = API.Me.Position;
-            if (tempPosition != currentPosition)
-            {
-                result += ("new Vector3(" + API.Me.Position.X + "f, " + API.Me.Position.Y + "f, " + API.Me.Position.Z + "f),");
-                currentPosition = tempPosition;
-            }
-            yield return 500;
-            index++;
-            if (index % 6 == 0)
-            {
-                if (!result.Equals("Vector3[] hotspots = new Vector3[]{{"))
-                {
-                    API.Print(result.Substring(0,result.Length - 1) + "}};");
-                }
-                else
-                {
-                    API.Print("No Hotzones Added Yet.  You may want to start moving!");
-                }
-            }
-        }
-        if (!result.Equals("Vector3[] hotspots = new Vector3[]{{"))
-        {
-            API.Print(result.Substring(0,result.Length - 1) + "}};");
-        }
-        else
-        {
-            API.Print("No Hotzones Were Added.  Please Move on a path in-game and the hotspot generator will build the mesh.");
-        }
-        yield break;
-    }
     
     // Method:          "InsertionSort(Vector3[] hotspots)"
     // What it does:       "Places all hotspots in direct point A to B line in order of distance from the player.  Will be rarely used.
-    private static Vector3[] InsertionSortHotspots(Vector3[] hotspots)
+    public static Vector3[] InsertionSortHotspots(Vector3[] hotspots)
     {
         Vector3 temp;
         for (int i = 0; i < hotspots.Length - 1; i++)
@@ -334,7 +299,7 @@ public class Mesh
     // Method:          "InsertionSort(Vector3[] hotspots)"
     // What it does:       "Places all hotspots in direct point A to B line in order of distance from a given position
     //                      To be mainly used with the GetMesh method.
-    private static Vector3[] InsertionSortHotspots(Vector3[] hotspots, Vector3 destination)
+    public static Vector3[] InsertionSortHotspots(Vector3[] hotspots, Vector3 destination)
     {
         Vector3 temp;
         for (int i = 0; i < hotspots.Length - 1; i++)
@@ -359,8 +324,9 @@ public class Mesh
                 // Why waste time parsing through them all if I am already right there?
         if (API.Me.Distance2DTo(destination) > 3f)
         {
+            SetCorrectMesh(destination);
             Vector3[] route = GetFastestRoute(destination);
-            API.Print(API.Me.Distance2DTo(route[0]));
+
             // Player is far away from custom mesh.. possible Stuck issues could occur. Issue warning!
             if (API.Me.Distance2DTo(route[0]) >= 15f)
             {
@@ -374,14 +340,14 @@ public class Mesh
                     Vector3[] route2 = GetFastestRoute(currentMesh[currentMesh.Length-1]);
                     for (int i = 0; i < route2.Length; i++)
                     {
-                        while(!API.CTM(route2[i]) && API.Me.Distance2DTo(route2[i]) > 1.5f)
+                        while(!API.CTM(route2[i]))
                         {
                             yield return 100;
                         }
                     }
                 }
                 // Finale MoveTo to special node at edge of mesh.
-                while(!API.MoveTo(route[route.Length - 1]) && API.Me.Distance2DTo(route[route.Length - 1]) > 1.5f)
+                while(!API.MoveTo(route[route.Length - 1]))
                 {
                     yield return 250;
                 }
@@ -392,18 +358,65 @@ public class Mesh
             // Navigating through CTM normally.
             for (int i = 0; i < result.Length - 1; i++)
             {
-                while(!API.CTM(result[i]) && API.Me.Distance2DTo(result[i]) > 1.5f)
+                API.Print(result[i]);
+                while(!API.CTM(result[i]))
                 {
                     yield return 100;
                 }
             }
         }
                     
-        while(!API.CTM(destination) && API.Me.Distance2DTo(destination) > 1.5f)
+        while(!API.CTM(destination))
         {
             yield return 250;
         }
         yield break;
+    }
+    
+    // Method:              "GetFastestRoute(Vector3 destination")
+    // What it Does         In case of a looped pathway, this will determine which one is shorter.  This is critical for mesh, but 
+    //                      not useful in a straight line.
+    public static Vector3[] GetFastestRoute(Vector3 destination)
+    {
+        // re-initialize
+        
+        return CreatePath(currentMesh, destination);
+        // sort hotspots so i can get closest 2.
+        //currentmesh = insertionsorthotspots(currentmesh);
+        
+        // to tally all the distances.
+        // float distance1 = 0.0f;
+        // float distance2 = 0.0f;
+        // vector3[] tempmesh = new vector3[currentmesh.length - 1];
+        // for (int i = 0; i < tempmesh.length; i++)
+        // {
+        //     tempmesh[i] = currentmesh[i+1];
+        // }
+
+        
+        // vector3[] path2 = createpath(tempmesh, destination);
+        // vector3 position = api.me.position;
+        
+        // for (int i = 0; i < path1.length; i++)
+        // {
+        //     distance1 = distance1 + path1[i].distance2d(position);
+        //     position = path1[i];
+        // }
+
+        // position = api.me.position;
+        // for (int i = 0; i < path2.length; i++)
+        // {
+        //     distance2 = distance2 + path2[i].distance2d(position);
+        //     position = path2[i];
+        // }
+        // if (distance1 < distance2)
+        // {
+        //     return path1;
+        // }
+        // else
+        // {
+        //     return path2;
+        // }
     }
     
     
@@ -425,32 +438,25 @@ public class Mesh
         int index = 0;
         int count = 0;
         bool noReset = true;
-        
         // Determining which hotspot is closest to destination.
-        Vector3 final = hotspotList[0];
-        for (int i = 1; i < hotspotList.Count - 1; i++)
-        {
-            if (hotspotList[i].Distance2D(destination) < final.Distance2D(destination))
-            {
-                final = hotspotList[i];
-            }
-        }
-        
+        Vector3 final = GetClosestNode(hotspots,destination);
+
         while(NotDoneSorting)
         {
             // closest to current node (first will be closest to player)
             closest = hotspotList[count];
-            for (int i = count + 1; i < hotspotList.Count - 1; i++)
+            for (int i = count; i < hotspotList.Count - 1; i++)
             {
-                if (hotspotList[i].Distance2D(position) < closest.Distance2D(position))
+                if (position.Distance2D(hotspotList[i]) < position.Distance2D(closest))
                 {
                     closest = hotspotList[i];
                     index = i;
                 }
             }
             
-            if (closest.Distance2D(position) > 10f && count != 0)
+            if (closest.Distance2D(position) > 15f && count != 0 && closest != final)
             {
+                // Removing Previous hotspot, then starting over.
                 hotspotList.RemoveAt(count - 1);
                 finalResult.Clear();
                 position = API.Me.Position;
@@ -473,9 +479,8 @@ public class Mesh
                 position = closest;
             }
             
-            
             // Determining if we reached the last hotspot;
-            if (closest == final && noReset)
+            if ((closest == final || closest.Distance2D(destination) <  7f) && noReset)
             {
                 NotDoneSorting = false;
             }
@@ -484,6 +489,7 @@ public class Mesh
         }
         // Adding Special node
         finalResult.Add(hotspots[hotspots.Length - 1]);
+        API.Print("The final result closest node is: " + finalResult[0].Distance2D(API.Me.Position));
         // Building new, smaller array
         Vector3[] result = new Vector3[finalResult.Count];
         for (int i = 0; i < result.Length; i++)
@@ -536,48 +542,51 @@ public class Mesh
         return result;
     }
     
-    // Method:              "GetFastestRoute(Vector3 destination")
-    // What it Does         In case of a looped pathway, this will determine which one is shorter.  This is critical for mesh, but 
-    //                      not useful in a straight line.
-    public static Vector3[] GetFastestRoute(Vector3 destination)
+    
+    // Method:             "HotspotGenerator(int seconds)"
+    // What it Does:        Run this method whilst running a path in the game to build the actual mesh.
+    public static IEnumerable<int> HotspotGenerator(int seconds)
     {
-        // Re-initialize
-        SetCorrectMesh(destination);
-        // Sort hotspots so I can get closest 2.
-        currentMesh = InsertionSortHotspots(currentMesh);
-        
-        // to tally all the distances.
-        float distance1 = 0.0f;
-        float distance2 = 0.0f;
-        Vector3[] tempMesh = new Vector3[currentMesh.Length - 1];
-        for (int i = 0; i < tempMesh.Length; i++)
-        {
-            tempMesh[i] = currentMesh[i+1];
-        }
-        
-        Vector3[] path1 = CreatePath(currentMesh, destination);
-        Vector3[] path2 = CreatePath(tempMesh, destination);
-        Vector3 position = API.Me.Position;
-        
-        for (int i = 0; i < path1.Length; i++)
-        {
-            distance1 = distance1 + path1[i].Distance2D(position);
-            position = path1[i];
-        }
+        API.DisableCombat = true;
+        string result = ("Vector3[] hotspots = new Vector3[]{{");
+        int count = seconds * 3;
+        int index = 0;
+        Vector3 currentPosition = API.Me.Position;
+        Vector3 tempPosition = currentPosition;
 
-        position = API.Me.Position;
-        for (int i = 0; i < path2.Length; i++)
+        while (index < count)
         {
-            distance2 = distance2 + path2[i].Distance2D(position);
-            position = path2[i];
+            // Verifying Current Position has changed from the last one.
+            tempPosition = API.Me.Position;
+            if (tempPosition != currentPosition)
+            {
+                result += ("new Vector3(" + API.Me.Position.X + "f, " + API.Me.Position.Y + "f, " + API.Me.Position.Z + "f),");
+                currentPosition = tempPosition;
+            }
+            yield return 333;
+            index++;
+            if (index % 9 == 0)
+            {
+                if (!result.Equals("Vector3[] hotspots = new Vector3[]{{"))
+                {
+                    API.Print(result.Substring(0, result.Length - 1) + "}};");
+                }
+                else
+                {
+                    API.Print("No Hotzones Added Yet.  You may want to start moving!");
+                }
+            }
         }
-        if (distance1 < distance2)
+        if (!result.Equals("Vector3[] hotspots = new Vector3[]{{"))
         {
-            return path1;
+            API.Print(result.Substring(0, result.Length - 1) + "}};");
         }
         else
         {
-            return path2;
+            API.Print("No Hotzones Were Added.  Please Move on a path in-game and the hotspot generator will build the mesh.");
         }
+        API.DisableCombat = false;
+        yield break;
     }
+
 }
